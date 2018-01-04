b0d01d254eedb02469bc2194d89ff97a8d67f9af
fernandonogueira2 <fernando.nogueira2@gmail.com>
2018-01-04 15:24:32 +1100

    Comment some parts of the file

--- salt_sync.sh
+++ salt_sync.sh
@@ -9,13 +9,13 @@ sudo rm -rf /srv/*
 sudo cp -R salt /srv
 
 ## copy formulas content into /srv/formulas
-sudo cp -R formulas /srv/formulas
+##sudo cp -R formulas /srv/formulas
 
 ## copy pillar content into /srv/pillar
-sudo cp -R pillar /srv/pillar
+##sudo cp -R pillar /srv/pillar
 
 # copy salt master conf into /etc/salt/
-sudo cp master /etc/salt/
+##sudo cp master /etc/salt/
 
 # restart salt master service and check it status
 sudo /etc/init.d/salt-master restart
