88e5f3954545efc8ca55af02cdea93ec77ffbc2e
fernandonogueira2 <fernando.nogueira2@gmail.com>
2018-02-27 15:33:34 +1100

    correcting IP on master script

--- salt_sync.sh
+++ salt_sync.sh
@@ -15,7 +15,7 @@ sudo cp -R formulas /srv/formulas
 sudo cp -R pillar /srv/pillar
 
 # copy salt master conf into /etc/salt/
-##sudo cp master /etc/salt/
+sudo cp master /etc/salt/
 
 # restart salt master service and check it status
 sudo /etc/init.d/salt-master restart
